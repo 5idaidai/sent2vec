697e57f856f90d01007b25d443a5c10fe7219f61
superjom <superjom@gmail.com>
2014-05-13 21:46:13 +0800

    add sent2vec

---
+++ src/sent2vec.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <cmath>
+#include "utils.h"
+#include "sent.h"
+#include "vocab.h"
+#include "windowtable.h"
+using namespace std;
+
+namespace sent2vec {
+//
+class Sent2Vec {
+public:
+    Sent2Vec(costr path="", 
+                int lenVec=50, 
+                int windowSize=2, 
+                int k=20, float alpha=0.1): \
+        path(path), lenVec(lenVec), windowSize(windowSize), k(k), alpha(alpha)
+    {
+        // init objects
+        vocab.init(lenVec);
+        sent.init(lenVec);
+        windowTable.init(windowSize, &vocab, path);
+        if(!path.empty())
+        {
+            createVocab();
+            createSent();
+            createWindowTable();
+        }
+    }
+
+    void createVocab()
+    {
+        vocab.initFromFile(path);
+    }
+
+    void createSent()
+    {
+        sent.initFromFile(path);
+    }
+
+    void createWindowTable()
+    {
+        windowTable.genTable();
+    }
+
+    // train from a file
+    void train(costr path)
+    {
+        ifstream infile(path.c_str());
+        if(!infile)
+        {
+            cout << "*ERROR: no such file :" << path << endl;
+            exit(-1);
+        }
+
+        string sentence;
+        while(getline(infile, sentence))
+        {
+            sentence = trim(sentence);
+            trainBySent(sentence);
+        }
+    }
+
+    float trainBySent(costr sentence)
+    {
+        float Jn = 0.0;
+        vector<vstr> windows = genWindowsFromSentence(sentence, windowSize);
+        if(windows.empty()) return 0.0;
+        // pointer to the real vector
+        Vec *v_p = &sent[sentence];
+
+        for(int wn=0; wn<windows.size(); ++wn)
+        {
+            vector<IndexType> wordIdxs;
+            // get word's id in vocabulary
+            string windowKey = genWindowKey(vocab, windows[wn]);
+            Vec h = vocab.getWindowVec(windows[wn]);
+            // generate noise samples
+            vstr samples = windowTable.getSamples(this->k);
+            // for positive sample
+            // e^(v^T h)
+            float e_vT_h = exp( v_p->dot(h));
+            Vec update_v = h / ( 1.0 + e_vT_h);
+            Vec update_h = (*v_p) / (1.0 + e_vT_h);
+            Jn += log( 1.0 / ( 1.0 + 1.0/e_vT_h));
+            vocab.updateWindow(windowKey, update_h, alpha);
+
+            for(int idx=0; idx<samples.size(); idx++)
+            {
+                // get noise's window vector
+                Vec n_h = vocab.getWindowVec(samples[idx]);
+                float e_vT_h = exp(v_p->dot(n_h));
+                float frac_e_v_h = 1.0 - 1.0 / (1.0 + e_vT_h);
+                update_v -= (n_h * frac_e_v_h);
+                Vec update_n_h = (*v_p) * (-1.0 * frac_e_v_h);
+                // update noise
+                vocab.updateWindow(samples[idx], update_n_h, alpha);
+                Jn += log( 1.0 / ( 1.0 + e_vT_h));
+            }
+
+            update_v /= (1.0 + this->k);
+
+            IndexType sentId = sent.index(sentence);
+            sent.updateVec(sentId, update_v, alpha);
+        }
+        return Jn;
+    }
+
+
+private:
+    string path;
+    int lenVec;
+    int k;
+    float alpha;
+    Vocab vocab;
+    Sent sent;
+    int windowSize;
+    WindowTable windowTable;
+};
+//
+}; // end namespace
+
+int main()
+{
+    using namespace sent2vec;
+    Sent2Vec s2v("1.sample");
+    s2v.train("1.sample");
+
+
+
+
+    return 0;
+}
